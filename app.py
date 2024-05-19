fc1112405a17a53d57b5d74cde3de0cddccc07aa
Dev Chaudhary <devc1829@gmail.com>
2024-05-19 16:03:20 +0530

    Developer1 story

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-<<<<<<< HEAD
 def sum(a,b,c):
     print(a+b+c)
-=======
 # Addition
 def sum(a,b,c):
     return a+b+c
->>>>>>> d156623870177061e8055836115758f52c986585
 
 def mul(a,b):
     return a*b
+
+
+def division(a,b):
+    return a/b
